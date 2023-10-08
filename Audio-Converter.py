2744e53bb128263feeb7539449e57ad5d9c5b6f4
BomberBeetle <gdreks@gmail.com>
2023-10-08 12:06:01 -0300

    fdafdsfdafdsafdsa

--- Audio-Converter.py
+++ Audio-Converter.py
@@ -1,29 +1,34 @@
-from PIL import Image
+import cv2
 import numpy as np
-from pydub import AudioSegment
 import json
+from scipy.io.wavfile import write
+import sounddevice as sd
+import time
+
+maxfreq = 4186
+minfreq = 27.5
+
+sps = 44100
+dur = 1.0
+att = 0.3
 
 objects = {}
 
 with open("objects.json", 'r') as file:
     objects = json.loads(file.read())
 
-
 for idx in objects.keys():
-    imagem = Image.open("object{}.png".format(idx))
-    dados_numericos = np.array(imagem)
+    imagem = cv2.imread("objects/object{}.png".format(idx))
+    imagem_gray = cv2.cvtColor(imagem, cv2.COLOR_RGBA2GRAY)
 
-    # Defina o número de canais, a largura da amostra e a taxa de quadros
-    numero_de_canais = 1  # Para áudio mono
-    largura_da_amostra = imagem.dtype.itemsize
-    frame_rate = 44100  # Você pode ajustar isso conforme necessário
+    avg_value, _,_,_ = cv2.mean(imagem_gray)
+    
+    freq = (avg_value - 0) * (maxfreq - 255) // (255 - 0) + minfreq
 
-    # Crie o objeto de áudio
-    audio = AudioSegment(
-        data=imagem_audio.tobytes(),
-        sample_width=largura_da_amostra,
-        frame_rate=frame_rate,
-        channels=numero_de_canais
-    )
-
-    audio.export('object{}.wav'.format(idx), format='wav')
+    each_sample_number = np.arange(dur* sps)
+    wave = np.sin(2 * np.pi * each_sample_number * freq / sps)
+    wave_atten = wave*att
+    
+    scaled = np.int16(wave_atten/np.max(np.abs(wave_atten)) * 32767)
+    write('objects/object{}.wav'.format(idx), sps, scaled)
+    
