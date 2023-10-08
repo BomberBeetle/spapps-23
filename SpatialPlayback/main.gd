83f25492ee3cfc8b8ef06c88082fa1977cf705f7
BomberBeetle <gdreks@gmail.com>
2023-10-08 13:56:01 -0300

    hummina hummina

--- SpatialPlayback/main.gd
+++ SpatialPlayback/main.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var speed = 0.25;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+
 	var SpatialObject = load("res://SpatialObject.tscn")
 	
 	var json_text = FileAccess.get_file_as_string("res://objects.json");
@@ -26,17 +26,29 @@ func _ready():
 			max_y = objs_dict[obj_idx]["y"]	
 		
 	for obj_idx in objs_dict.keys():
+		
 		var img = Image.load_from_file("res://objects/object{i}.png".format({"i": obj_idx}))
 		var texture = ImageTexture.create_from_image(img)
+		
+		var wav = load("res://objects/object{i}.wav".format({"i": obj_idx}))
+		wav.set_loop_end((wav.data.size()-1)/8)
+		wav.set_loop_mode(AudioStreamWAV.LOOP_PINGPONG)
+		
+		
 		var new_object = SpatialObject.instantiate()
 		var size = objs_dict[obj_idx]["w"] * objs_dict[obj_idx]["h"]
 		new_object.get_node("Sprite3D").texture = texture;
 		
+		new_object.get_node("AudioStreamPlayer3D").stream = wav
+		
 		new_object.transform.origin = Vector3((objs_dict[obj_idx]["x"]-((max_x - min_x)/2) - min_x)/100, -(objs_dict[obj_idx]["y"]-(max_y/2))/100, -size/20000)
 		add_child(new_object)
+	
+		new_object.get_node("AudioStreamPlayer3D").stream_paused = false
+		new_object.get_node("AudioStreamPlayer3D").play()
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Camera3D.position = Vector3($Camera3D.position.x, $Camera3D.position.y, $Camera3D.position.z-speed*delta);
+	$Camera3D.position = Vector3($Camera3D.position.x, $Camera3D.position.y, $Camera3D.position.z+speed*delta);
 	pass
